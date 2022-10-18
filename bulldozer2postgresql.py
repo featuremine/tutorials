33bb0e2fd788d512434ff2aea50abc4a580332f3
Ivan <ivan.gonzalez@featuremine.com>
2022-10-18 17:17:38 -0300

    progress

--- bulldozer2postgresql.py
+++ bulldozer2postgresql.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import psycopg2
+import argparse
 import subprocess
 import os
 import time
@@ -12,66 +13,70 @@ my_env = os.environ.copy()
 my_env["PATH"] = os.path.join(src_dir, 'build', 'dependencies', 'build', 'yamal') + ":" + my_env["PATH"]
 my_env["YAMALCOMPPATH"] = os.path.join(src_dir, 'build', 'dependencies', 'build', 'bulldozer', 'package', 'lib', 'yamal', 'modules')
 
-proc_comp = subprocess.Popen(['yamal-run', '-c', os.path.join(src_dir, 'build', 'dependencies', 'src', 'bulldozer', 'samples', 'coinbase_l2_ore_ytp.ini'), '-s', 'main'],
-                        env=my_env)
+if __name__ == "__main__":
+   parser = argparse.ArgumentParser()
+   parser.add_argument("--password", help="postgreSQL database password", required=False, default="")
+   args = parser.parse_args()
+   proc_comp = subprocess.Popen(['yamal-run', '-c', os.path.join(src_dir, 'build', 'dependencies', 'src', 'bulldozer', 'samples', 'coinbase_l2_ore_ytp.ini'), '-s', 'main'],
+                           env=my_env)
 
-#patch to delay initialization
-time.sleep(1)
-
-proc_stats = subprocess.Popen(['yamal-stats', 'ore_coinbase_l2.ytp', '-f', '-b'],
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        env=my_env)
-
-#patch to delay initialization
-time.sleep(3)
-
-conn = psycopg2.connect(database="myusername", user = "myusername", password = "mypassword", host = "127.0.0.1", port = "5432")
-print("Opened database successfully")
-cur = conn.cursor()
-cur.execute("""
-CREATE TABLE IF NOT EXISTS bulldozer_rate
-(
-   rate_id SERIAL PRIMARY KEY NOT NULL,
-   timestamp TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() at time zone 'utc'),
-   rate INT NOT NULL
-)
-""")
- # commit the changes
-conn.commit()
-
-run = True
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    run = False
-
-signal.signal(signal.SIGINT, signal_handler)
-
-discard = 4
-while run:
+   #patch to delay initialization
    time.sleep(1)
-   line = proc_stats.stdout.readline()
-   if not line:
-      break
-   rate=line.decode('utf-8').rstrip()
-   if rate and discard > 0:
-      discard -= 1
-      continue
-   print(rate)
-   cur.execute(f"""
-   INSERT INTO bulldozer_rate (rate) VALUES
-   ({rate})
+
+   proc_stats = subprocess.Popen(['yamal-stats', 'ore_coinbase_l2.ytp', '-f', '-b'],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE,
+                           env=my_env)
+
+   #patch to delay initialization
+   time.sleep(3)
+
+   conn = psycopg2.connect(database="myusername", user = "myusername", password =  args.password, host = "127.0.0.1", port = "5432")
+   print("Opened database successfully")
+   cur = conn.cursor()
+   cur.execute("""
+   CREATE TABLE IF NOT EXISTS bulldozer_rate
+   (
+      rate_id SERIAL PRIMARY KEY NOT NULL,
+      timestamp TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() at time zone 'utc'),
+      rate INT NOT NULL
+   )
    """)
+   # commit the changes
    conn.commit()
 
+   run = True
+   def signal_handler(sig, frame):
+      print('You pressed Ctrl+C!')
+      run = False
 
-conn.close()
+   signal.signal(signal.SIGINT, signal_handler)
 
-proc_comp.send_signal(subprocess.signal.SIGINT)
-proc_comp.wait()
+   discard = 4
+   while run:
+      time.sleep(1)
+      line = proc_stats.stdout.readline()
+      if not line:
+         break
+      rate=line.decode('utf-8').rstrip()
+      if rate and discard > 0:
+         discard -= 1
+         continue
+      print(rate)
+      cur.execute(f"""
+      INSERT INTO bulldozer_rate (rate) VALUES
+      ({rate})
+      """)
+      conn.commit()
 
-proc_stats.send_signal(subprocess.signal.SIGINT)
-proc_stats.wait()
-            
-proc_stats.stdout.close()
-proc_stats.stderr.close()
+
+   conn.close()
+
+   proc_comp.send_signal(subprocess.signal.SIGINT)
+   proc_comp.wait()
+
+   proc_stats.send_signal(subprocess.signal.SIGINT)
+   proc_stats.wait()
+               
+   proc_stats.stdout.close()
+   proc_stats.stderr.close()
