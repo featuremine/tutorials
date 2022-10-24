5cea7cd9723ac2822e8cd71518375e69f6c77cd7
Ivan <ivan.gonzalez@featuremine.com>
2022-10-24 15:51:31 -0300

    works tutorial1 and tutorial2

--- market-data2_1.sh
+++ market-data2_1.sh
@@ -2,4 +2,5 @@
 
 cd /opt
 yamal-run -c /usr/local/lib/yamal/modules/bulldozer/samples/coinbase_l2_ore_ytp.ini -s main &
-#yamal-run -m syncer -o syncer --config /opt/syncer-source.ini --section main
+sleep 1
+yamal-run -m syncer -o syncer --config /opt/syncer-source.ini --section main
