a62e24fe9a8489fce9ca37e28558f477dbb1d36b
Ivan <ivan.gonzalez@featuremine.com>
2022-10-25 10:24:28 -0300

    Add book and trades

--- market-data2_2.sh
+++ market-data2_2.sh
@@ -4,3 +4,4 @@ DB_NAME="${POSTGRES_DB:-${POSTGRES_USER}}"
 yamal-run -m syncer -o syncer --config /opt/syncer-sink.ini --section main &
 python3 bulldozer2postgresql.py --database ${DB_NAME} --user ${POSTGRES_USER} --ytp /opt/ore_coinbase_l2.ytp &
 python3 bars2postgresql.py --license /opt/test.lic --ytp /opt/ore_coinbase_l2.ytp --peer sink/source/feed_handler --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD" --database ${DB_NAME} --user ${POSTGRES_USER} &
+python3 book2postgresql.py --license /opt/test.lic --ytp /opt/ore_coinbase_l2.ytp --peer sink/source/feed_handler --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD" --database ${DB_NAME} --user ${POSTGRES_USER} &
