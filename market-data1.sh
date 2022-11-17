4a297e32206d5e0339396b3244db69053ae9f029
Ivan <ivan.gonzalez@featuremine.com>
2022-11-17 09:31:33 -0300

    works

--- market-data1.sh
+++ market-data1.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd /opt
 yamal-run -c /usr/local/lib/yamal/modules/bulldozer/samples/coinbase_l2_ore_ytp.ini -s main &
-python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp &
-python3 bars2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
+python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp
+#python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp &
+#python3 bars2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
