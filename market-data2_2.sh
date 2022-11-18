b409908992acb9061540c910b47f2fc24ba31c8a
Ivan <ivan.gonzalez@featuremine.com>
2022-11-18 17:51:04 -0300

    fix

--- market-data2_2.sh
+++ market-data2_2.sh
@@ -2,6 +2,5 @@
 
 cd /opt
 yamal-run -m syncer -o syncer --config /opt/syncer-sink.ini --section main &
-python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp
 python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp &
 python3 bars2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
