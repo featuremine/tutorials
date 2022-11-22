5a9e3f844d38d4222aeeb4653add517239224fe1
Ivan <ivan.gonzalez@featuremine.com>
2022-11-22 11:35:52 -0300

    Add postgres host and port env variables with default values

--- market-data1.sh
+++ market-data1.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+HOST="${POSTGRES_HOST:=host.docker.internal}"
+PORT="${POSTGRES_PORT:=5432}"
+
 cd /opt
 yamal-run -c /usr/local/lib/yamal/modules/bulldozer/samples/coinbase_l2_ore_ytp.ini -s main &
-python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp &
-python3 bars2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
+python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp &
+python3 bars2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
--- market-data2_2.sh
+++ market-data2_2.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+HOST="${POSTGRES_HOST:=host.docker.internal}"
+PORT="${POSTGRES_PORT:=5432}"
+
 cd /opt
 yamal-run -m syncer -o syncer --config /opt/syncer-sink.ini --section main &
-python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp &
-python3 bars2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host host.docker.internal --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
+python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp &
+python3 bars2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
