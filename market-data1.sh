6d3ee28e57111c2ef36b8ec33a239368145484fa
Ivan <ivan.gonzalez@featuremine.com>
2022-11-22 16:20:35 -0300

    Add postgress database name as env variable

--- market-data1.sh
+++ market-data1.sh
@@ -2,8 +2,9 @@
 
 HOST="${POSTGRES_HOST:=host.docker.internal}"
 PORT="${POSTGRES_PORT:=5432}"
+NAME="${POSTGRES_NAME:=${POSTGRES_USER}}"
 
 cd /opt
 yamal-run -c /usr/local/lib/yamal/modules/bulldozer/samples/coinbase_l2_ore_ytp.ini -s main &
-python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp &
-python3 bars2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
+python3 bulldozer2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp &
+python3 bars2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
--- market-data2_2.sh
+++ market-data2_2.sh
@@ -2,8 +2,9 @@
 
 HOST="${POSTGRES_HOST:=host.docker.internal}"
 PORT="${POSTGRES_PORT:=5432}"
+NAME="${POSTGRES_NAME:=${POSTGRES_USER}}"
 
 cd /opt
 yamal-run -m syncer -o syncer --config /opt/syncer-sink.ini --section main &
-python3 bulldozer2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp &
-python3 bars2postgresql.py --database ${POSTGRES_USER} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
+python3 bulldozer2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp &
+python3 bars2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-BTC,ADA-USD"
