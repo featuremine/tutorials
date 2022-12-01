2dbc03c2dd5db28376823b4d5842f98f38c2229e
Ivan <ivan.gonzalez@featuremine.com>
2022-12-01 18:59:10 -0300

    shutdown if some of the commands failed

--- market-data1.sh
+++ market-data1.sh
@@ -6,6 +6,6 @@ NAME="${POSTGRES_NAME:=${POSTGRES_USER}}"
 PERIOD="${BARS_PERIOD:=10}"
 
 cd /opt
-yamal-run -c /opt/bulldozer_coinbase.ini -s main &
-python3 bulldozer2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp &
-python3 bars2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-USD,DOGE-USD,USDT-USD" --period ${BARS_PERIOD}
+yamal-run -c /opt/bulldozer_coinbase.ini -s main || >&2 echo "yamal-run failed" && shutdown now &
+python3 bulldozer2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp || >&2 echo "bulldozer2postgresql.py failed" && shutdown now &
+python3 bars2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-USD,DOGE-USD,USDT-USD" --period ${BARS_PERIOD} || >&2 echo "bars2postgresql.py failed" && shutdown now
