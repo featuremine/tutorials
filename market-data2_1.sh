20a5a4d4a6698030e4b019e9e1ac767f33566842
Ivan Gonzalez <106779693+fm-ivang@users.noreply.github.com>
2022-12-02 14:37:23 -0300

    Fix run script (#16)
    * shutdown if some of the commands failed
    * kill the main script to exit the docker container

--- market-data2_1.sh
+++ market-data2_1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd /opt
-yamal-run -c /opt/bulldozer_coinbase.ini -s main &
+yamal-run -c /opt/bulldozer_coinbase.ini -s main || >&2 echo "yamal-run bulldozer failed" && kill -9 $$ &
 sleep 1
-yamal-run -m syncer -o syncer --config /opt/syncer-source.ini --section main
+yamal-run -m syncer -o syncer --config /opt/syncer-source.ini --section main || >&2 echo "yamal-run syncer failed"
