a297d62a88679d36421b3f28cf289dfa0a83898a
Maxim Trokhimtchouk <max@featuremine.com>
2022-12-06 16:42:21 -0500

    maybe works

--- ordersgui/test.py
+++ ordersgui/test.py
@@ -4,11 +4,26 @@ from collections import defaultdict
 
 
 class ReferenceData(object):
+    class State(object):
+        def __init__(self) -> None:
+            self.venuesSecurities = defaultdict(set)
+            self.venuesNames = defaultdict(dict)
+            self.securities = defaultdict(dict)
+            self.accounts = set()   
+
+        def update(self, delta):
+            for k, v in delta.venuesSecurities.items():
+                self.venuesSecurities[k] &= v
+            for k, v in delta.venuesNames.items():
+                self.venuesNames[k] = v
+            for k, v in delta.securities.items():
+                self.securities[k] = v
+            self.accounts &= delta.accounts
+
     def __init__(self, yamal: str, cfg: dict) -> None:
-        self.venuesSecurities = defaultdict(set)
-        self.venuesNames = defaultdict(dict)
-        self.securities = defaultdict(dict)
-        self.accounts = set()
+        self.state = ReferenceData.State()
+        self.delta = ReferenceData.State()
+
         self.callbacks = []
         self.parser = {
             cfg['venue_channel']: schemas.reference.VenueData,
@@ -23,10 +38,6 @@ class ReferenceData(object):
         self.callbacks.append(clb)
 
     def _seq_clbck(self, peer, channel, time, data):
-        venuesSecurities = defaultdict(set)
-        venuesNames = defaultdict(dict)
-        securities = defaultdict(dict)
-        accounts = set()
         chname = channel.name()
         if not chname in self.parser:
             return
@@ -34,7 +45,7 @@ class ReferenceData(object):
         print(d)
         if 'venue' in d['message']:
             v = d['message']['venue']
-            item = venuesNames[v['identifier']]
+            item = self.delta.venuesNames[v['identifier']]
             item['code'] = v['code']
             if 'exdest' in v:
                 item['exdest'] = v['exdest']
@@ -43,36 +54,32 @@ class ReferenceData(object):
                 item['label'] = v['code']
         elif 'venueSecurityAttribute' in d['message']:
             vsa = d['message']['venueSecurityAttribute']
-            venuesSecurities[vsa['venueID']].add(vsa['securityId'])
+            self.delta.venuesSecurities[vsa['venueID']].add(vsa['securityId'])
         elif 'securityDefinition' in d['message']:
             item = d['message']['securityDefinition']
-            securities[item['identifier']] = {
+            self.delta.securities[item['identifier']] = {
                 'symbol': item['symbol']
             }
         elif 'account' in d['message']:
-            accounts.add(d['message']['account']['identifier'])
-        for c in self.callbacks:
-            c(venuesSecurities, venuesNames, securities, accounts)
-        for k, v in venuesSecurities.items():
-            self.venuesSecurities[k] &= v
-        for k, v in  venuesNames.items():
-            self.venuesNames[k] = v
-        for k, v in  securities.items():
-            self.securities[k] = v
-        self.accounts &= accounts
+            self.delta.accounts.add(d['message']['account']['identifier'])
+
 
     def poll(self, limit=None):
+        self.delta = ReferenceData.State()
         count = 0
         while self.seq.poll() and (not limit or count <= limit):
             count += 1
+        for c in self.delta.callbacks:
+            c(self.delta)
 
+        self.state.update(self.delta)
 
 if __name__ == '__main__':
-    def prt(venuesSecurities, venuesNames, securities, accounts):
-        print(venuesSecurities)
-        print(venuesNames)
-        print(securities)
-        print(accounts)
+    def prt(delta):
+        print(delta.venuesSecurities)
+        print(delta.venuesNames)
+        print(delta.securities)
+        print(delta.accounts)
 
     cfg = {
         'venue_channel': 'venu',
