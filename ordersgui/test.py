f0dc0e75dfaa8caf252c7776bcef7ec3e92b082c
Maxim Trokhimtchouk <max@featuremine.com>
2022-12-06 17:13:17 -0500

    progress

--- ordersgui/test.py
+++ ordersgui/test.py
@@ -1,14 +1,23 @@
 from yamal import ytp
 from conveyor.utils import schemas
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 
+class MarketData(object):
+    State = namedtuple('State', ['bid', 'ask'])
+    def __init__(self) -> None:
+        self.prices = defaultdict(MarketData.State)
+    
+    def subscribe(self, imnts: dict[tuple[int, int], tuple[str,str]]) -> None:
+        if self.prices:
+            return
+        # call extractor here on another thread
 
 class ReferenceData(object):
     class State(object):
         def __init__(self) -> None:
             self.venuesSecurities = defaultdict(set)
-            self.venuesNames = defaultdict(dict)
-            self.securities = defaultdict(dict)
+            self.venuesNames = defaultdict(namedtuple('Venue', ['label', 'code', 'exdest']))
+            self.securities = defaultdict(namedtuple('Security', ['symbol']))
             self.accounts = set()   
 
         def update(self, delta):
@@ -46,40 +55,34 @@ class ReferenceData(object):
         if 'venue' in d['message']:
             v = d['message']['venue']
             item = self.delta.venuesNames[v['identifier']]
-            item['code'] = v['code']
+            item.code = v['code']
             if 'exdest' in v:
-                item['exdest'] = v['exdest']
-                item['label'] = f"{v['code']}/{v['exdest']}"
+                item.exdest = v['exdest']
+                item.label = f"{v['code']}/{v['exdest']}"
             else:
-                item['label'] = v['code']
+                item.exdest = None
+                item.label = v['code']
         elif 'venueSecurityAttribute' in d['message']:
             vsa = d['message']['venueSecurityAttribute']
             self.delta.venuesSecurities[vsa['venueID']].add(vsa['securityId'])
         elif 'securityDefinition' in d['message']:
             item = d['message']['securityDefinition']
-            self.delta.securities[item['identifier']] = {
-                'symbol': item['symbol']
-            }
+            self.delta.securities[item['identifier']].symbol = item['symbol']
         elif 'account' in d['message']:
             self.delta.accounts.add(d['message']['account']['identifier'])
-
 
     def poll(self, limit=None):
         self.delta = ReferenceData.State()
         count = 0
         while self.seq.poll() and (not limit or count <= limit):
             count += 1
-        for c in self.callbacks:
-            c(self.delta)
 
         self.state.update(self.delta)
 
+        for c in self.callbacks:
+            c(self.delta)
+
 if __name__ == '__main__':
-    def prt(delta):
-        print(delta.venuesSecurities)
-        print(delta.venuesNames)
-        print(delta.securities)
-        print(delta.accounts)
 
     cfg = {
         'venue_channel': 'venu',
@@ -88,5 +91,26 @@ if __name__ == '__main__':
     }
 
     refdata = ReferenceData('symb.ytp', cfg=cfg)
+    mrkdata = MarketData()
+
+    def prt(delta):
+        print(delta.venuesSecurities)
+        print(delta.venuesNames)
+        print(delta.securities)
+        print(delta.accounts)
+
     refdata.add_callback(prt)
+
+    def mktSubscribe(delta):
+        imnts = {}
+        for venid, securities in delta.venueSecurities.items():
+            venue = refdata.state.venuesNames[venid]
+            market = venue.exdest if venue.exdest else venue.code
+            for secid in securities:
+                symbol = refdata.state.securities[secid].symbol
+                imnts[(venid, secid)] = (market, symbol)
+        mktSubscribe(imnts)
+
+    refdata.add_callback(mktSubscribe)
+
     refdata.poll()
