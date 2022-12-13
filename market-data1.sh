b32699b1f186e1e981ba5edf3f4f92ce09b3df47
Ivan <ivan.gonzalez@featuremine.com>
2022-12-13 10:28:10 -0300

    bulldozer rate works

--- market-data1.sh
+++ market-data1.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 HOST="${POSTGRES_HOST:=host.docker.internal}"
-PORT="${POSTGRES_PORT:=5432}"
-NAME="${POSTGRES_NAME:=${POSTGRES_USER}}"
-PERIOD="${BARS_PERIOD:=10}"
+PORT="${POSTGRES_PORT:=9200}"
 
 cd /opt
 yamal-run -c /opt/bulldozer_coinbase.ini -s main || >&2 echo "yamal-run failed" && kill -9 $$ &
-python3 bulldozer2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp || >&2 echo "bulldozer2postgresql.py failed"
+python3 bulldozer2elasticsearch.py --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp || >&2 echo "bulldozer2elasticsearch.py failed"
 #python3 bars2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-USD,DOGE-USD,USDT-USD" --period ${BARS_PERIOD} || >&2 echo "bars2postgresql.py failed"
