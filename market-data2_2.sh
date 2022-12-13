819351638daaac9252df946ffcf64bbd3bab32f7
Ivan <ivan.gonzalez@featuremine.com>
2022-12-13 18:04:04 -0300

    tutorial 2 progress

--- market-data2_2.sh
+++ market-data2_2.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-HOST="${POSTGRES_HOST:=host.docker.internal}"
-PORT="${POSTGRES_PORT:=5432}"
-NAME="${POSTGRES_NAME:=${POSTGRES_USER}}"
+HOST="${DATABASE_HOST:=host.docker.internal}"
+PORT="${DATABASE_PORT:=9200}"
 PERIOD="${BARS_PERIOD:=10}"
 
 cd /opt
 yamal-run -m syncer -o syncer --config /opt/syncer-sink.ini --section main || >&2 echo "yamal-run syncer failed" && kill -9 $$ &
-python3 bulldozer2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp || >&2 echo "bulldozer2postgresql.py failed" && kill -9 $$ &
-python3 bars2postgresql.py --database ${POSTGRES_NAME} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-USD,DOGE-USD,USDT-USD" --period ${BARS_PERIOD} || >&2 echo "bars2postgresql.py failed"
+python3 bulldozer2elasticsearch.py --host ${DATABASE_HOST} --port ${DATABASE_PORT} --ytp /opt/ore_coinbase_l2.ytp || >&2 echo "bulldozer2elasticsearch.py failed" && kill -9 $$ &
+python3 bars2elasticsearch.py --host ${DATABASE_HOST} --port ${DATABASE_PORT} --ytp /opt/ore_coinbase_l2.ytp --markets "coinbase" --imnts "BTC-USD,ETH-USD,DOGE-USD,USDT-USD" --period ${BARS_PERIOD} || >&2 echo "bars2elasticsearch.py failed"
