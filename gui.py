e6bdf555bf1eca4b5429fea934f6c2fa2a546db8
Ivan <ivan.gonzalez@featuremine.com>
2022-12-27 17:10:53 -0300

    Update layout

--- gui.py
+++ gui.py
@@ -161,6 +161,12 @@ if args.no_gui:
     exit()
 
 ## UI
+def padded_row():
+    return ui.row().style('margin:1em;')
+
+def expansion_bar(name):
+    return ui.expansion(name).classes('w-full').props(add='switch-toggle-side').style('background-color: #e5e8e8')
+
 UNAVAILABLE = '-'
 
 def update_prices():
@@ -170,103 +176,155 @@ def update_prices():
     asklabel.set_text(p['askpx'])
 
 with ui.header().style('background-color: #3874c8').props('elevated'):
-    ui.icon('monetization_on')
-    ui.label('Featuremine Trading GUI').style('width:15em;align-items:left;text-align:left;')
-    with ui.row().style('margin-start:auto;margin-end:right;align-items:right;'):
-        selectAccount = ui.select([]).style('width:10em;height:1em;align-items:right;text-align:right;')
+    with ui.column():
+        with ui.row():
+            ui.icon('monetization_on').style('top: 50%;transform: translateY(-10%)')
+            ui.label('Featuremine Trading GUI')
+    with ui.column().style('margin-start:auto;margin-end:right;align-items:right;'):
+        selectAccount = ui.select([]).style('width:10em;height:1em;').props(add='borderless label=Account')
 
-with ui.row().style('margin-start:auto;margin-end:auto;align-items:center;'):
-    ui.label('Market').style('width:10em;align-items:center;text-align:center;')
-    ui.label('Instrument').style('width:10em;align-items:center;text-align:center;')
-with ui.row().style('margin-start:auto;margin-end:auto;align-items:center;'):
-    def update_select_securities(market):
-        selectSecurity.value = None
-        selectSecurity.options = {}
-        where = refdata.state.venuesSecurities.get(market, [])
-        for sid in where:
-            print(sid)
-            selectSecurity.options[sid] = refdata.state.securities[sid].symbol
-        selectSecurity.update()
+with expansion_bar('orders BUY/SELL'):
+    with padded_row():
+        with ui.column():
+            with ui.row():
+                def update_select_securities(market):
+                    selectSecurity.value = None
+                    selectSecurity.options = {}
+                    where = refdata.state.venuesSecurities.get(market, [])
+                    for sid in where:
+                        print(sid)
+                        selectSecurity.options[sid] = refdata.state.securities[sid].symbol
+                    selectSecurity.update()
 
-    selectMarket = ui.select({}, on_change=lambda s: update_select_securities(s.value)).style('width:10em;align-items:center;text-align:center;')
-    selectSecurity = ui.select({}, on_change=update_prices).style('width:10em;align-items:center;text-align:center;')
+                selectMarket = ui.select({}, on_change=lambda s: update_select_securities(s.value)).style('width:10em;align-items:center;text-align:center;').props(add='label=Market')
+                selectSecurity = ui.select({}, on_change=update_prices).style('width:10em;align-items:center;text-align:center;').props(add='label=Instrument')
+                
+        with ui.column():
+            with ui.row().style('align-items:center;'):
+                ui.label('bid price').style('width:10em;align-items:center;text-align:center;')
+                ui.label('ask price').style('width:10em;align-items:center;text-align:center;')
+            with ui.row().style('align-items:center;'):
+                bidlabel = ui.label(UNAVAILABLE).style('width:10em;align-items:center;text-align:center;')
+                asklabel = ui.label(UNAVAILABLE).style('width:10em;align-items:center;text-align:center;')
 
-with ui.row().style('margin-start:auto;margin-end:auto;align-items:center;'):
-    ui.label('bid price').style('width:10em;align-items:center;text-align:center;')
-    ui.label('ask price').style('width:10em;align-items:center;text-align:center;')
-
-with ui.row().style('margin-start:auto;margin-end:auto;align-items:center;'):
-    bidlabel = ui.label(UNAVAILABLE).style('width:10em;align-items:center;text-align:center;')
-    asklabel = ui.label(UNAVAILABLE).style('width:10em;align-items:center;text-align:center;')
-
-def update_qty():
-    if notionalcheckbox.value:
-        if is_number(qtyin.value) and is_number(pricein.value):
-            qtyout.set_text("Quantity: {:.6f}".format(float(qtyin.value)/float(pricein.value)))
-        else:
-            qtyout.set_text(f"Quantity: -")
-    else:
-        if is_number(qtyin.value) and is_number(pricein.value):
-            qtyout.set_text("Notional: {:.6f}".format(float(qtyin.value)*float(pricein.value)))
-        else:
-            qtyout.set_text(f"Notional: -")
-
-with ui.row().style('margin-start:auto;margin-end:auto;align-items:center;'):
-    pricein = ui.input(label='Price', placeholder='0.00', on_change=update_qty).style('width:8em;align-items:center;text-align:center;')
-    with ui.column().style('margin-start:auto;margin-end:auto;align-items:center;'):
-        def update_askbid_checkbox(check):
-            if check.sender == bidcheckbox and check.value:
-                askcheckbox.set_value(False)
-                pricein.props(add='readonly')
-            elif check.sender == askcheckbox and check.value:
-                bidcheckbox.set_value(False)
-                pricein.props(add='readonly')
+    def update_qty():
+        if notionalswitch.value:
+            if is_number(qtyin.value) and is_number(pricein.value):
+                qtyout.set_text("Quantity: {:.6f}".format(float(qtyin.value)/float(pricein.value)))
             else:
-                pricein.props(remove='readonly')
+                qtyout.set_text(f"Quantity: -")
+        else:
+            if is_number(qtyin.value) and is_number(pricein.value):
+                qtyout.set_text("Notional: {:.6f}".format(float(qtyin.value)*float(pricein.value)))
+            else:
+                qtyout.set_text(f"Notional: -")
+
+    with padded_row():
+        with ui.column():
+            pricein = ui.input(label='Price', placeholder='0.00', on_change=update_qty).style('width:10em;align-items:center;text-align:center;')
+        with ui.column():
+            def update_askbid_checkbox(check):
+                if check.sender == bidcheckbox and check.value:
+                    askcheckbox.set_value(False)
+                    pricein.props(add='readonly')
+                elif check.sender == askcheckbox and check.value:
+                    bidcheckbox.set_value(False)
+                    pricein.props(add='readonly')
+                else:
+                    pricein.props(remove='readonly')
+
+            bidcheckbox = ui.checkbox('bid', on_change=lambda c: update_askbid_checkbox(c)).style('width:5em;height:1em;align-items:center;text-align:center;margin-top:1em;')
+            askcheckbox = ui.checkbox('ask', on_change=lambda c: update_askbid_checkbox(c)).style('width:5em;height:1em;align-items:center;text-align:center;')
         
-        bidcheckbox = ui.checkbox('bid', on_change=lambda c: update_askbid_checkbox(c)).style('width:5em;height:1em;align-items:center;text-align:center;')
-        askcheckbox = ui.checkbox('ask', on_change=lambda c: update_askbid_checkbox(c)).style('width:5em;height:1em;align-items:center;text-align:center;')
+        with ui.column():
+            with ui.row():
+                def switch_qty(notional):
+                    qtyin.view.label = 'Notional' if notional else 'Quantity'
+                    qtyin.update()
+                    update_qty()
+                        
+                qtyin = ui.input(label='Quantity', placeholder='0.00', on_change=update_qty).style('width:10em;align-items:center;text-align:center;')
+                with ui.column():
+                    qtyout = ui.label('Notional: -').style('width:10em;align-items:center;text-align:left;margin-top:2em;')
 
-with ui.row().style('margin-start:auto;margin-end:auto;align-items:center;'):
-    def switch_qty(notional):
-        qtyin.view.label = 'Notional' if notional else 'Quantity'
-        qtyin.update()
-        update_qty()
-            
-    qtyin = ui.input(label='Quantity', placeholder='0.00', on_change=update_qty).style('width:8em;align-items:center;text-align:center;')
-    notionalcheckbox = ui.checkbox('notional', on_change=lambda c: switch_qty(c.value)).style('width:5em;height:1em;align-items:center;text-align:center;')
-    
-with ui.row().style('margin-start:auto;margin-end:auto;align-items:center;'):
-    qtyout = ui.label('Notional: -').style('width:10em;align-items:center;text-align:center;')
+                with ui.column():
+                    notionalswitch = ui.switch('notional', on_change=lambda c: switch_qty(c.value)).style('margin-top:1em;')                
 
-with ui.row().style('margin-start:auto;margin-end:auto;align-items:center;'):
-    def parse_order(side):
-        if not selectAccount.value:
-            ui.notify('please select an account')
-            return
-        elif not selectMarket.value:
-            ui.notify('please select a market')
-            return
-        elif not selectSecurity.value:
-            ui.notify('please select a security')
-            return
-        elif not is_number(qtyin.value):
-            ui.notify('please select a valid quantity')
-            return
-        elif not is_number(pricein.value):
-            ui.notify('please select a valid price')
-            return
-            
-        orders.send(orders.limit(
-            accid=int(selectAccount.value), secid=int(selectSecurity.value),
-            venid=int(selectMarket.value), side=side, ordpx=float(pricein.value),
-            qty=float(qtyin.value)))
-        ui.notify('An order was sent')
-    ui.button('buy', on_click=lambda: parse_order('buy')).style('width:9em;align-items:center;text-align:center;').props('color=green')
-    ui.button('sell', on_click=lambda: parse_order('sell')).style('width:9em;align-items:center;text-align:center;')
+    with padded_row():
+        def parse_order(side):
+            if not selectAccount.value:
+                ui.notify('please select an account')
+                return
+            elif not selectMarket.value:
+                ui.notify('please select a market')
+                return
+            elif not selectSecurity.value:
+                ui.notify('please select a security')
+                return
+            elif not is_number(qtyin.value):
+                ui.notify('please select a valid quantity')
+                return
+            elif not is_number(pricein.value):
+                ui.notify('please select a valid price')
+                return
+                
+            orders.send(orders.limit(
+                accid=int(selectAccount.value), secid=int(selectSecurity.value),
+                venid=int(selectMarket.value), side=side, ordpx=float(pricein.value),
+                qty=float(qtyin.value)))
+            ui.notify('An order was sent')
+        ui.button('buy', on_click=lambda: parse_order('buy')).style('width:10em;align-items:center;text-align:center;').props('color=green')
+        ui.button('sell', on_click=lambda: parse_order('sell')).style('width:10em;align-items:center;text-align:center;')
 
-with ui.expansion('orders', icon='work').classes('w-full'):
-    orderslog = ui.log(max_lines=100).classes('w-full h-16')
+
+selected = []
+
+with expansion_bar('orders list'):
+    with padded_row():
+        with ui.column():
+            def cancel_orders(b):
+                global selected
+                indices = [i for i, x in enumerate(selected) if x]
+                for i in sorted(indices, reverse=True):
+                    table.options['rowData'].pop(i)
+                table.update()
+                selected = [ x for i,x in enumerate(selected) if not x]
+
+            ui.button('cancel', on_click=cancel_orders).style('width:10em').props('color=red')
+                
+    with padded_row():
+        table = ui.table(options={
+            'defaultColDef': {
+                'minWidth': 100,
+                'filter': True,
+                'cellStyle': {'display': 'flex ','justify-content': 'center'},
+                'headerClass': 'font-bold'
+            }, 
+            'columnDefs': [
+                {'headerName': '', 'field': 'enabled', 'cellRenderer': 'checkboxRenderer'},
+                {'headerName': 'Order', 'field': 'order'},
+                {'headerName': 'Account', 'field': 'account'},
+                {'headerName': 'Security', 'field': 'security'},
+                {'headerName': 'Venue', 'field': 'venue'},
+                {'headerName': 'Side', 'field': 'side'},
+                {'headerName': 'Price', 'field': 'price'},
+                {'headerName': 'Quantity', 'field': 'quantity'},
+            ],
+            'rowData': [
+                {'enabled': False, 'order': 1001, 'account': 1001, 'security': 1001, 'venue':1001, 'side':'buy', 'price':1.1, 'quantity':2.2 },
+                {'enabled': False, 'order': 1002, 'account': 1001, 'security': 1001, 'venue':1001, 'side':'buy', 'price':1.1, 'quantity':2.2 },
+                {'enabled': False, 'order': 1003, 'account': 1001, 'security': 1001, 'venue':1001, 'side':'buy', 'price':1.1, 'quantity':2.2 },
+            ],
+        }).style('margin:0;padding:0;height:100vh;width:100%;')
+        selected = [False] * len(table.options['rowData'])
+        for col_def in table.view.options.columnDefs:
+            col_def.cellClass = ['text-2xl','text-white-500']
+        table.view.theme = 'ag-theme-balham-dark'
+                
+        def handle_change(sender, msg):
+            selected[msg['rowIndex']] = msg['value']
+
+        table.view.on('cellValueChanged', handle_change)
 
 ## Market Data
 refdata = reference.ReferenceData(seq=seqref, cfg=cfg)
@@ -313,8 +371,9 @@ def mktSubscribe(delta):
 refdata.add_callback(mktSubscribe)
 
 def update_orders_ui(requests, responses):
-    for r in requests:
-        orderslog.push(f"order id {r['message']['strg']['new']['strgOrdID']}")
+    pass
+    #for r in requests:
+    #    orderslog.push(f"order id {r['message']['strg']['new']['strgOrdID']}")
 
 orders.add_callback(update_orders_ui)
 
