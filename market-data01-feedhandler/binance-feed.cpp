47447450d0134b1bb91e76ef1022e8682c305178
Maxim Trokhimtchouk <max@featuremine.com>
2023-09-05 16:14:46 -0400

    adding readme

--- market-data01-feedhandler/binance-feed.cpp
+++ market-data01-feedhandler/binance-feed.cpp
@@ -7,12 +7,12 @@
 int main()
 {
 
-    std::unique_ptr<WebSocket> ws(WebSocket::from_url("ws://localhost:8126/foo"));
+    std::unique_ptr<easywsclient::WebSocket> ws(easywsclient::WebSocket::from_url("ws://localhost:8126/foo"));
     assert(ws);
     ws->send("goodbye");
     ws->send("hello");
-    while (ws->getReadyState() != WebSocket::CLOSED) {
-        WebSocket::pointer wsp = &*ws; // <-- because a unique_ptr cannot be copied into a lambda
+    while (ws->getReadyState() != easywsclient::WebSocket::CLOSED) {
+        easywsclient::WebSocket::pointer wsp = &*ws; // <-- because a unique_ptr cannot be copied into a lambda
         ws->poll();
         ws->dispatch([wsp](const std::string & message) {
             printf(">>> %s\n", message.c_str());
