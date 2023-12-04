5024a8c9e73c26ba35792888c67517055e72d672
Andrés Rangel <andres.rangel@featuremine.com>
2023-12-04 16:59:01 -0500

    adding python scripts to packages

--- market-data02-consolidated/book-dump.py
+++ market-data02-consolidated/book-dump.py
@@ -8,12 +8,13 @@
 
 import argparse
 import extractor
+from functools import partial
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--ytp", help="YTP file name", required=True)
-    parser.add_argument("--channel", help="YTP channel", required=True)
-    parser.add_argument("--live", help="Run in live mode", action="store_true")
+    parser.add_argument("--prefix", help="Prefix for market data channels", required=True)
+    parser.add_argument("--securities", help="List of securities", required=True, nargs='*', default=[], type=str)
     parser.add_argument('--graphviz', help="Visualize graph of instantiated nodes", action='store_true')
 
     args = parser.parse_args()
@@ -21,11 +22,7 @@ if __name__ == '__main__':
     graph = extractor.system.comp_graph()
     op = graph.features
 
-    if args.live:
-        upds = op.seq_ore_live_split(args.ytp, (args.channel,))
-    else:
-        upds = op.seq_ore_sim_split(args.ytp, (args.channel,))
-
+    upds = op.seq_ore_live_split(args.ytp, tuple([args.prefix + sec for sec in args.securities]))
     levels = [op.book_build(upd, 1) for upd in upds]
 
     if args.graphviz:
@@ -33,9 +30,6 @@ if __name__ == '__main__':
         g = graph_viz(graph)
         g.render(view=True)
     else:
-        for level in levels:
-            graph.callback(level, lambda frame: print(frame))
-        if args.live:
-            graph.stream_ctx().run_live()
-        else:
-            graph.stream_ctx().run()
+        for sec, level in zip(args.securities, levels):
+            graph.callback(level, partial(lambda frame, security: print(security + '\n', frame), security=sec))
+        graph.stream_ctx().run_live()
